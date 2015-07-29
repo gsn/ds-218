d2d8392a1836bdc5125f76cb409b64807c9ab85c
noogen <tommywhen@yahoo.com>
2015-07-29 11:43:09 -0500

    remove printable coupon

--- asset/218/storeApp.js
+++ asset/218/storeApp.js
@@ -47,9 +47,13 @@
             storeRequired: true,
             caseInsensitiveMatch: true
           })
-          .when('/coupons/printable', {
+          /*.when('/coupons/printable', {
             templateUrl: gsn.getContentUrl('/views/coupons-printable.html'),
             storeRequired: true,
+            caseInsensitiveMatch: true
+          })*/
+          .when('/coupons/printable', {
+            redirectTo: '/coupons',
             caseInsensitiveMatch: true
           })
           .when('/coupons/digital', {
